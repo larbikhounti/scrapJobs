d19eeed9436df3b49ab7b45935d784e8d7370d94
mohamed khounti <30549055+larbikhounti@users.noreply.github.com>
2020-06-30 01:35:03 +0100

    fixing bugs

--- myserver.js
+++ myserver.js
@@ -1,6 +1,7 @@
 const request = require('request');
 const cheerio = require("cheerio");
 const express = require('express');
+const bodyParser = require('body-parser')
 const cors = require('cors');
 const app = express();
 
@@ -30,6 +31,8 @@ let $2;
 let globlres;
 let detailsRespond;
 let fulldescreption = '';
+
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cors());
 app.set("port", port);
 app.listen(port, function () {
@@ -49,9 +52,11 @@ app.get('/', function (req, res) {
   res.sendfile('./welcome/welcome.html');
 }) // end of get the jobs route
 
-app.get('/details/:url', function (req, res) {
+app.post('/details/', function (req, res) {
   detailsRespond = res;
-  getJobDetails(req.params.url);
+  let url = req.body.url;
+
+  getJobDetails(url);
 }) // end of get the jobs route
 
 
@@ -105,9 +110,9 @@ function addData() {
 }
 
 function getJobDetails(url) {
-
+  
   request(url, function (error, response, body) {
-
+    
     console.error('error:', error); // Print the error if one occurred
     console.log('statusCode:', response.statusCode); // Print the response status code if a response was received
     //console.log('body:', body); // Print the HTML for the Google homepage.
@@ -115,7 +120,7 @@ function getJobDetails(url) {
       $2 = cheerio.load(body);
       jobDetails.link = $2("div.icl-u-lg-hide>a.icl-Button--primary").attr("href");
       jobDetails.title = $2("div.jobsearch-JobInfoHeader-title-container").text();
-      jobDetails.Description[i] = $2("div#jobDescriptionText").text()
+      jobDetails.Description= $2("div#jobDescriptionText").text()
 
 
       detailsRespond.json(jobDetails);
